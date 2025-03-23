f571480e3584168068b6e869586787d28930cc02
EC2 Default User <ec2-user@ip-10-0-1-217.ap-northeast-1.compute.internal>
2025-03-23 06:08:12 +0000

    AWS Farget用の設定動作確認済

--- createxyz-project/next.config.js
+++ createxyz-project/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     return [
       {
         source: '/api/shops',
-        destination: 'http://localhost:8080/shops' // bearsunday の正しいURLに置き換えてください
+	destination: 'http://localhost/api/shops' // nginxがフロントエンド内でapiをリダイレクト
       },
     ];
   },
